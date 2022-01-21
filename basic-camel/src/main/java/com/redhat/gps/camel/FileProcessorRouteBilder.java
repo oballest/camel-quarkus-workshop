468b65cc7a586f4b584c0b846a75015cb38bf07c
Oscar Ballesteros <oballest@redhat.com>
2022-01-21 08:59:43 -0500

    Ajuste dev file

--- basic-camel/src/main/java/com/redhat/gps/camel/FileProcessorRouteBilder.java
+++ basic-camel/src/main/java/com/redhat/gps/camel/FileProcessorRouteBilder.java
@@ -1,5 +1,7 @@
 package com.redhat.gps.camel;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class FileProcessorRouteBilder extends RouteBuilder{
@@ -27,8 +29,29 @@ public class FileProcessorRouteBilder extends RouteBuilder{
             .to("file:out_files/change-filter").
         end();
 
+        from("file:in_files/camel-msg")
+        .log("Cargando Archivo ${header.CamelFileName} ")
+        .setProperty("Propiedad1").constant("Propiedad Exchange")
+        .setHeader("Header1").constant("Header1")
+        .process(new Processor() {
+
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                String body = exchange.getIn().getBody(String.class);
+                String header = exchange.getIn().getHeader("Header1",String.class);
+                String property = exchange.getProperty("Propiedad1", String.class);
+                System.out.println("body="+body+" header="+header+" propiedad="+property);
 
 
+                exchange.getOut().setBody(body+" "+header);
+
+            }
+            
+        })
+        .log("body = ${body}")
+        .log("Header = ${header.Header1}")
+        .log("Property = ${exchangeProperty.Propiedad1}")
+        .to("log:com.redhat.gps.camel.loggger");
         
         
     }
